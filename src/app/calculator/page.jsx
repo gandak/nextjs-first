202d7df881d07d49f237430b8b6a560f7862984a
24LP3548 24LP3548 <24LP3548@G304-CC0228.local>
2024-12-26 12:13:14 +0800

    calculator operations

--- src/app/calculator/page.jsx
+++ src/app/calculator/page.jsx
@@ -1,25 +1,107 @@
 "use client";
+import styles from "./calculatorStyles.module.css";
 const { useState } = require("react");
 
 const Calculator = () => {
-  const [number, setNumber] = useState(0);
+  const [screen, setScreen] = useState("");
+  let [operationCounter, setOperationCounter] = useState(0);
+  const [operation, setOperation] = useState("");
+  let nums = [];
 
-  const changeNumber = (x) => {
-    setNumber(x);
+  const buttonClicked = (x) => {
+    switch (x) {
+      case "+":
+        setOperation("+");
+        break;
+      case "-":
+        setOperation("-");
+        break;
+      case "/":
+        setOperation("/");
+        break;
+      case "*":
+        setOperation("*");
+        break;
+      case "%":
+        setOperation("%");
+        break;
+    }
+
+    if (x === "+" || x === "-" || x === "*" || x === "/" || x === "%")
+      setOperationCounter(operationCounter + 1);
+
+    if (operationCounter === 1 || operationCounter === 0) {
+      setScreen(screen + x);
+      // operationCounter = 1;
+    }
+
+    if (operationCounter === 2) {
+      for (let i = 0; i < screen.length; i++) {
+        if (screen[i] === "+") {
+          nums = screen.split("+");
+          console.log(nums);
+          setScreen(Number(nums[0]) + Number(nums[1]) + "x");
+          setOperationCounter(1);
+        }
+      }
+    }
+  };
+
+  const operaionHandler = () => {
+    if (screen.includes("+")) {
+      nums = screen.split("+");
+      setScreen(Number(nums[0]) + Number(nums[1]));
+    }
+    if (screen.includes("-")) {
+      nums = screen.split("-");
+      setScreen(Number(nums[0]) - Number(nums[1]));
+    }
+    if (screen.includes("*")) {
+      nums = screen.split("*");
+      setScreen(Number(nums[0]) * Number(nums[1]));
+    }
+    if (screen.includes("/")) {
+      nums = screen.split("/");
+      setScreen(Number(nums[0]) / Number(nums[1]));
+    }
+    if (screen.includes("%")) {
+      nums = screen.split("%");
+      setScreen(Number(nums[0]) % Number(nums[1]));
+    }
+  };
+
+  // const addHandler = () => {
+
+  // }
+
+  const clearScreen = () => {
+    setScreen("");
   };
   return (
-    <div>
-      <p>Number: {number}</p>
-      <button onClick={() => changeNumber(1)}>1</button>
-      <button onClick={() => changeNumber(2)}>2</button>
-      <button onClick={() => changeNumber(3)}>3</button>
-      <button onClick={() => changeNumber(4)}>4</button>
-      <button onClick={() => changeNumber(5)}>5</button>
-      <button onClick={() => changeNumber(6)}>6</button>
-      <button onClick={() => changeNumber(7)}>7</button>
-      <button onClick={() => changeNumber(8)}>8</button>
-      <button onClick={() => changeNumber(9)}>9</button>
-      <button onClick={() => changeNumber(0)}>0</button>
+    <div className={styles.calculatorContainer}>
+      <div className={styles.screenContainer}>
+        <p>{screen}</p>
+      </div>
+
+      <div className={styles.buttonContainer}>
+        <button onClick={() => buttonClicked(1)}>1</button>
+        <button onClick={() => buttonClicked(2)}>2</button>
+        <button onClick={() => buttonClicked(3)}>3</button>
+        <button onClick={() => buttonClicked(4)}>4</button>
+        <button onClick={() => buttonClicked(5)}>5</button>
+        <button onClick={() => buttonClicked(6)}>6</button>
+        <button onClick={() => buttonClicked(7)}>7</button>
+        <button onClick={() => buttonClicked(8)}>8</button>
+        <button onClick={() => buttonClicked(9)}>9</button>
+        <button onClick={() => buttonClicked(0)}>0</button>
+        <button onClick={() => buttonClicked("+")}>+</button>
+        <button onClick={() => buttonClicked("-")}>-</button>
+        <button onClick={() => buttonClicked("*")}>*</button>
+        <button onClick={() => buttonClicked("/")}>/</button>
+        <button onClick={() => buttonClicked("%")}>%</button>
+        <button onClick={operaionHandler}>=</button>
+        <button onClick={clearScreen}>C</button>
+      </div>
     </div>
   );
 };
--- src/app/sudo/page.jsx
+++ src/app/sudo/page.jsx
@@ -3,21 +3,18 @@
 import Square from "@/components/Square";
 import "./boxstyle.css";
 
-// import { numbers } from "./numbers";
-
 const { useState } = require("react");
 
 const BoxFunction = () => {
   const [numbers, setNumbers] = useState([2, 3, 1, 5, 6, 4, 9, 7, 8]);
 
   const IncreaseFunction = () => {
-    const x = numbers.sort();
-    setNumbers([...x]);
+    // const x = numbers.sort();
+    setNumbers([...numbers.sort()]);
   };
-
   const DecreaseFunction = () => {
-    const x = numbers.sort((a, b) => b - a);
-    setNumbers([...x]);
+    // const x = numbers.sort((a, b) => b - a);
+    setNumbers([...numbers.sort((a, b) => b - a)]);
   };
 
   return (
